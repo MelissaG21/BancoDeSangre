9cd37aaa879c17f93d3ac9392bb29a5ad02e769b
moonormal <a334259@uach.mx>
2021-11-17 19:28:14 -0700

    Se agregan roles con rbac

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
 const schema = mongoose.Schema({
   _correo: String,
   _password: String,
   _tipoUsuario: String,
-  _salt:String
+  _salt:String,
+  _profiles: [{
+    type: mongoose.Schema.ObjectId, ref: 'Profile'
+  }]
 });
 
 class User{
@@ -48,7 +52,16 @@ class User{
     this._salt = v;
   }
 
+  get profiles(){
+    return this._profiles;
+  }
+
+  set profiles(v){
+      this._profiles = v;
+  }
+
 }
 
 schema.loadClass(User);
+schema.plugin(mongoosePaginate);
 module.exports = mongoose.model('User', schema);
